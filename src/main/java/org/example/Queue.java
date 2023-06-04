68e5ec4890b35e39a40b8889131e078be8ede2f2
Chanho Kim <cksgh1565@gmail.com>
2023-06-04 17:56:22 +0900

    test: head and tail of queue

--- src/main/java/org/example/Queue.java
+++ src/main/java/org/example/Queue.java
@@ -1,8 +1,37 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
 
+    List<Integer> List_init_Values = new ArrayList<Integer>();
+
+    public Queue(){
+
+    }
+
+    public Queue(List<Integer> init_values) {
+
+        for (Integer init_value : init_values) {
+            List_init_Values.add(init_value);
+            System.out.println(init_value);
+        }
+
+    }
     public int size() {
-        return 0;
+        return List_init_Values.size();
+    }
+
+    public Integer head() {
+        if(List_init_Values.size() == 0) return null;
+
+        return List_init_Values.get(List_init_Values.size()-1);
+    }
+
+    public Integer tail() {
+        if(List_init_Values.size() == 0) return null;
+
+        return List_init_Values.get(0);
     }
 }
--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -2,9 +2,21 @@ import org.example.Queue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 큐
+ *
+ * - 빈 배열로 생성
+ * - 아이템 몇개를 넣어서 생성가능하도록 ( 맨 뒤에가 헤드)
+ * - 처음에 넣은 값이 처음에 나오도록
+ * - 가장 머리의 녀석 확인, 꼬리녀석도 확인 가능하게 head, tail
+ * - 큐의 길이를 알고싶음
+ * - 더 생각나면 추가
+ */
 public class QueueTest {
 
     @Test
@@ -13,7 +25,23 @@ public class QueueTest {
         Queue queue = new Queue();
 
         // Assert
-        Assertions.assertEquals(queue.size(), 0);
+        Assertions.assertEquals(0,queue.size());
+        Assertions.assertEquals(null, queue.head());
+        Assertions.assertEquals(null, queue.tail());
+
+    }
+
+    @Test
+    public void Queue_Init_With_Values() {
+        // Arrange, Act
+        Queue queue = new Queue(new ArrayList<Integer>(Arrays.asList(1,2,3)));
+
+        // Act
+        Integer head_value = queue.head();
+
+        // Assertion
+        Assertions.assertEquals(3, queue.size());
+        Assertions.assertEquals(3, head_value);
 
     }
 
