e27a4fd51fd67c08f4970d8cbbf408abdfd33d4b
Chanho Kim <cksgh1565@gmail.com>
2023-06-04 18:04:48 +0900

    test: push values

--- src/main/java/org/example/Queue.java
+++ src/main/java/org/example/Queue.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Queue {
 
-    List<Integer> List_init_Values = new ArrayList<Integer>();
+    List<Integer> queue = new ArrayList<Integer>();
 
     public Queue(){
 
@@ -14,24 +14,28 @@ public class Queue {
     public Queue(List<Integer> init_values) {
 
         for (Integer init_value : init_values) {
-            List_init_Values.add(init_value);
+            queue.add(init_value);
             System.out.println(init_value);
         }
 
     }
     public int size() {
-        return List_init_Values.size();
+        return queue.size();
     }
 
     public Integer head() {
-        if(List_init_Values.size() == 0) return null;
+        if(queue.size() == 0) return null;
 
-        return List_init_Values.get(List_init_Values.size()-1);
+        return queue.get(queue.size()-1);
     }
 
     public Integer tail() {
-        if(List_init_Values.size() == 0) return null;
+        if(queue.size() == 0) return null;
 
-        return List_init_Values.get(0);
+        return queue.get(0);
+    }
+
+    public void push(int i) {
+        queue.add(0, i);
     }
 }
--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -10,11 +10,11 @@ import java.util.List;
 /**
  * 큐
  *
- * - 빈 배열로 생성
- * - 아이템 몇개를 넣어서 생성가능하도록 ( 맨 뒤에가 헤드)
- * - 처음에 넣은 값이 처음에 나오도록
- * - 가장 머리의 녀석 확인, 꼬리녀석도 확인 가능하게 head, tail
- * - 큐의 길이를 알고싶음
+ * - 빈 배열로 생성 v
+ * - 아이템 몇개를 넣어서 생성가능하도록 ( 맨 뒤에가 헤드) v
+ * - 처음에 넣은 값이 처음에 나오도록 v
+ * - 가장 머리의 녀석 확인, 꼬리녀석도 확인 가능하게 head, tail v
+ * - 큐의 길이를 알고싶음 v
  * - 더 생각나면 추가
  */
 public class QueueTest {
@@ -44,6 +44,38 @@ public class QueueTest {
         Assertions.assertEquals(3, head_value);
         Assertions.assertEquals(1, queue.tail());
 
+    }
+
+    @Test
+    public void Queue_push_One_Value() {
+        // Arrange
+        Queue queue = new Queue();
+
+        // Act
+        queue.push(4);
+
+        // Assert
+        Assertions.assertEquals(4, queue.head());
+        Assertions.assertEquals(4, queue.tail());
+        Assertions.assertEquals(1, queue.size());
+    }
+
+    @Test
+    public void Queue_push_Many_Values() {
+        // Arrange
+        Queue queue = new Queue();
+
+        // Act
+        queue.push(4);
+        queue.push(5);
+        queue.push(6);
+        queue.push(7);
+        queue.push(8);
+
+        // Assert
+        Assertions.assertEquals(4, queue.head());
+        Assertions.assertEquals(8, queue.tail());
+        Assertions.assertEquals(5, queue.size());
     }
 
 }
